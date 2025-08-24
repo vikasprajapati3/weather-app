6dcf1bd8d65ba8a1f969b7ef9bc337164bab5195
Vikas <vikasrjc7@gmail.com>
2025-08-24 20:06:52 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/weather-app/',
+})
 
 
-export default {
-  base: "/weather-app/", // your GitHub repo name
-};
